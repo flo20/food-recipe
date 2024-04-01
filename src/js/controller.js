bb7615103a0f0cd4ffcbcec5f51b6f58955754ad
flo20 <mawuaniipa@gmail.com>
2024-04-01 20:59:14 +0300

    Fetch data from API

--- src/js/controller.js
+++ src/js/controller.js
@@ -7,3 +7,20 @@ const timeout = function (s) {
     }, s * 1000);
   });
 };
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886?key=0e9551ed-f542-4f78-9e10-e9cf108846e2'
+    );
+
+    const dataFetching = await res.json();
+    const { data } = dataFetching;
+
+    if (!res.ok) throw new Error(`${res.statusText}`);
+  } catch (error) {
+    alert(error);
+  }
+};
+
+showRecipe();
