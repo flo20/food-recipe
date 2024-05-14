ec4accd3a4e3e4df11370b5451df2660cfaaf0b6
flo20 <mawuaniipa@gmail.com>
2024-05-14 14:36:48 +0300

    Replace render method with update method

--- src/js/config.js
+++ src/js/config.js
@@ -1,2 +1,4 @@
-export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
 export const TIMEOUT_SECONDS = 10;
+export const API_KEY = 'f7bfcc0d-598a-4b4e-8e63-de5a8d127b2c';
+export const RESULTS_PER_PAGE = 10;
